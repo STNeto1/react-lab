4569aaddde35ad72ac821da1faf976052d5c35fe
STNeto1 <gfreitasneto18@gmail.com>
2022-10-19 19:21:58 -0300

    toolkit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
+import { Provider } from 'react-redux'
+import { store } from '../src/app/store'
 import Counter from '../src/components/Counter'
 
 const Home: NextPage = () => {
   return (
-    <main className="container mt-5 flex items-center justify-center h-screen">
-      <Counter />
-    </main>
+    <Provider store={store}>
+      <main className="container mt-5 flex items-center justify-center h-screen">
+        <Counter />
+      </main>
+    </Provider>
   )
 }
 
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,19 +1,22 @@
-import { useCounter } from '../lib/counter'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../app/features/counter/counterSlice'
+import { RootState } from '../app/store'
 
 export default function Counter() {
-  const { count, inc, dec } = useCounter()
+  const count = useSelector((state: RootState) => state.counter.count)
+  const dispatch = useDispatch()
 
   return (
     <div className="flex items-center gap-4">
       <button
-        onClick={dec}
+        onClick={() => dispatch(decrement())}
         className="bg-red-500 text-white px-4 py-2 rounded-lg mr-2"
       >
         -
       </button>
       <p className="text-2xl">{count}</p>
       <button
-        onClick={inc}
+        onClick={() => dispatch(increment())}
         className="bg-green-500 text-white px-4 py-2 rounded-lg ml-2"
       >
         +
